dbc321a6261aefd5b40223dfb52537cc64044ed5
Lorena <77021882+loresgarcia@users.noreply.github.com>
2025-03-19 12:49:42 -0300

    ajustando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme/theme"; 
-import GlobalStyle from "./theme/GlobalStyle"; 
+import React from "react";
 import Button from "./components/Button";
 import ToggleButton from "./components/ToggleButton";
 import styled from "styled-components";
-
+import { useTheme } from "./context/ThemeContext";
 
 const AppContainer = styled.main`
     background-color: ${({ theme }) => theme.colors.background.primary};
@@ -24,23 +21,17 @@ const ButtonContainer = styled.div`
 `;
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false); 
-  const theme = isDarkMode ? darkTheme : lightTheme; 
-
-  const toggleTheme = () => setIsDarkMode(!isDarkMode); 
+  const { theme, isDarkMode, toggleTheme } = useTheme();
 
   return (
-    <StyledThemeProvider theme={theme}> 
-      <GlobalStyle />
-      <AppContainer>
-        <ButtonContainer>
-          <ToggleButton onClick={toggleTheme}>
-            {isDarkMode ? "Mudar para tema claro" : "Mudar para tema escuro"}
-          </ToggleButton>
-          <Button>Botão acessível</Button>
-        </ButtonContainer>
-      </AppContainer>
-    </StyledThemeProvider>
+    <AppContainer theme={theme}>
+      <ButtonContainer>
+        <ToggleButton onClick={toggleTheme}>
+          {isDarkMode ? "Mudar para tema claro" : "Mudar para tema escuro"}
+        </ToggleButton>
+        <Button>Botão acessível</Button>
+      </ButtonContainer>
+    </AppContainer>
   );
 }
 
